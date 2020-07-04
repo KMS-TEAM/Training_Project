2588068a05cff05904c9a85a62befbe8885c336a
lacie-life <00sao00ios00@gmail.com>
2020-07-04 21:56:10 +0700

    add something stupid

--- PyQt/NewTestApp/main.py
+++ PyQt/NewTestApp/main.py
@@ -1,14 +1,12 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from MainWindowGrid import Ui_MainWindow
+from MainWindow2 import Ui_MainWindow
 from PyQt5.QtGui import QIcon
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QIcon(":/recs/i.jpg"))
     MainWindow = QtWidgets.QMainWindow()
-    MainWindow.setWindowIcon(QIcon(":/recs/i.jpg"))
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
