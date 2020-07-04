596ee9034884085732d93fcff48a8ba94922b931
lacie-life <00sao00ios00@gmail.com>
2020-07-04 21:45:21 +0700

    add something stupid

--- PyQt/NewTestApp/MainWindowGrid.py
+++ PyQt/NewTestApp/MainWindowGrid.py
@@ -15,6 +15,9 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/recs/i.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
@@ -24,6 +27,17 @@ class Ui_MainWindow(object):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
         self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setSizeIncrement(QtCore.QSize(15, 15))
+        font = QtGui.QFont()
+        font.setPointSize(30)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        font.setStyleStrategy(QtGui.QFont.PreferAntialias)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 0, 0);")
+        self.label.setFrameShape(QtWidgets.QFrame.Box)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
         self.gridLayout = QtWidgets.QGridLayout()
@@ -51,6 +65,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.label.clear)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -61,3 +76,4 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "1"))
         self.pushButton_3.setText(_translate("MainWindow", "3"))
         self.pushButton_4.setText(_translate("MainWindow", "4"))
+
--- PyQt/NewTestApp/main.py
+++ PyQt/NewTestApp/main.py
@@ -2,11 +2,17 @@ import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from MainWindowGrid import Ui_MainWindow
+from PyQt5.QtGui import QIcon
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QIcon(":/recs/i.jpg"))
     MainWindow = QtWidgets.QMainWindow()
+    MainWindow.setWindowIcon(QIcon(":/recs/i.jpg"))
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
+
+
