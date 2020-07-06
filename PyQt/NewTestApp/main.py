f86b559ff5cf39fbe01016a708ad9073c33ba1e3
lacie-life <00sao00ios00@gmail.com>
2020-07-06 10:42:29 +0700

    add GroupBox and don't know how to use

--- PyQt/NewTestApp/main.py
+++ PyQt/NewTestApp/main.py
@@ -1,13 +1,13 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from MainWindowWhatis import Ui_Dialog
+from ButtonGroup import Ui_Form
 from PyQt5.QtGui import QIcon
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
+    ui = Ui_Form()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
