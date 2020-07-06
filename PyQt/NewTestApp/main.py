fff6b22639b16d15136373876d8a55cfaf98a4fc
lacie-life <00sao00ios00@gmail.com>
2020-07-06 10:00:34 +0700

    add CheckBox ui

--- PyQt/NewTestApp/main.py
+++ PyQt/NewTestApp/main.py
@@ -1,13 +1,13 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from MainWindow2 import Ui_MainWindow
+from MainWindowWhatis import Ui_Dialog
 from PyQt5.QtGui import QIcon
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_Dialog()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
