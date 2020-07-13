427e4a129b2011a1739a0c5c45617c8ecd7832f8
lacie-life <00sao00ios00@gmail.com>
2020-07-13 14:31:48 +0700

    update ui

--- PyQt/mqttClient/main.py
+++ PyQt/mqttClient/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from ui_mainwindow import Ui_MainWindow
+from ui_mainwindow import Ui_mqttClient
 from PyQt5.QtGui import QIcon
 
 import PyQt5
@@ -18,7 +18,7 @@ from pymongo import MongoClient
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_mqttClient()
         self.ui.setupUi(self)
 
         self.setupGUI_Iots()
@@ -33,9 +33,9 @@ class MainWindow(QMainWindow):
         self.mqttc.subscribe("demo/test", qos=0)
         self.mqttc.loop_start()
 
-        self.ui.label.setText("Host :" + "127.0.0.1")
-        self.ui.label_2.setText("Port :" + "1883")
-        self.ui.label_3.setText("Topic :" + "demo/test")
+        self.ui.label.setText("Host :" + " 127.0.0.1")
+        self.ui.label_2.setText("Port :" + " 1883")
+        self.ui.label_3.setText("Topic :" + " demo/test")
 
         self.timer_alarm = QTimer(self)
         self.timer_alarm.timeout.connect(self.print)
@@ -46,18 +46,28 @@ class MainWindow(QMainWindow):
         self.client = MongoClient('localhost', 27017)
 
         self.db = self.client["mydatabase"]
-        self.collection = self.db["customers"]
-        for x in self.collection.find():
-            print(x)
+        self.collection = self.db["WeatherStation"]
+    #    for x in self.collection.find():
+    #       print(x)
 
     def on_iot_message(self, mqttc, obj, msg):
         print('Update IoTs ...')
         self.data = msg.payload
-        print(self.data)
+#        print(self.data)
         self.ui.label_4.setText("Message : " + str(self.data))
         self.convertdata()
-        data = {"data": str(self.data)}
-        print(data)
+        self.printData()
+        data = {
+           "WindDirection" : self.windDirection,
+           "WindSpeedAverage" : self.windSpeedAverage,
+           "WindSpeedMax" : self.windSpeedMax,
+            "Temperature" : self.temperature,
+            "RainfallOneHour" : self.rainFallOneHour,
+            "RainfallOneDay" : self.rainFallOneDay,
+            "Humidity" : self.humidity,
+            "BarPressure" : self.barPressure
+        }
+#        print(data)
         self.result = self.collection.insert_one(data)
 
     def on_iot_publish(self, mqttc, obj, mid):
@@ -72,17 +82,64 @@ class MainWindow(QMainWindow):
         print("convert data function")
         self.id = ''
         self.sensorData = ''
-
         for i in range(temp.find('<')+1, temp.find('>')):
         #    print(temp[i])
             self.id = self.id + temp[i]
         for i in range(temp.find('>')+1, temp.find('*')):
             self.sensorData = self.sensorData + temp[i]
-
         print(self.id)
         print(self.sensorData)
 
+        WindDirection = ''
+        for i in range(1, 4):
+            WindDirection = WindDirection + self.sensorData[i]
+        self.windDirection = int(WindDirection)
+    #    print(WindDirection)
 
+        WindSpeedAverage = ''
+        for i in range(5,8):
+            WindSpeedAverage = WindSpeedAverage + self.sensorData[i]
+        self.windSpeedAverage = 0.44704*(int(WindSpeedAverage))
+
+        WindSpeedMax = ''
+        for i in range(9,12):
+            WindSpeedMax = WindSpeedMax + self.sensorData[i]
+        self.windSpeedMax = 0.44704*(int(WindSpeedMax))
+
+        Temperature = ''
+        for i in range(13,16):
+            Temperature = Temperature +self.sensorData[i]
+        self.temperature = (int(Temperature)-32.00)*5.00/9.00
+
+        RainfallOneHour = ''
+        for i in range(17,20):
+            RainfallOneHour = RainfallOneHour + self.sensorData[i]
+        self.rainFallOneHour = (int(RainfallOneHour))*25.40*0.01
+
+        RainfallOneDay = ''
+        for i in range(21,24):
+            RainfallOneDay = RainfallOneDay + self.sensorData[i]
+        self.rainFallOneDay = (int(RainfallOneDay))*25.04*0.01
+
+        Humidity = ''
+        for i in range(25,27):
+            Humidity = Humidity + self.sensorData[i]
+        self.humidity = int(Humidity)
+
+        BarPressure = ''
+        for i in range(28,33):
+            BarPressure = BarPressure + self.sensorData[i]
+        self.barPressure = int(BarPressure) / 10.00
+
+    def printData(self):
+        self.ui.WindDirection.setText("Wind Direction: " + str(self.windDirection))
+        self.ui.WindSpeedAverage.setText("Wind Speed Average: " + str(self.windSpeedAverage))
+        self.ui.WindSpeedMax.setText(("Wind Speed Max: " + str(self.windSpeedMax)))
+        self.ui.Temperature.setText(("Temperature: " + str(self.temperature)))
+        self.ui.RainfallOneHour.setText(("Rain fall One Hour: " + str(self.rainFallOneHour)))
+        self.ui.RainfallOneDay.setText(("Rain fall One Day: " + str(self.rainFallOneDay)))
+        self.ui.Humidity.setText(("Humidity: " + str(self.humidity)))
+        self.ui.BarPressure.setText(("Bar Pressure: " + str(self.barPressure)))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
