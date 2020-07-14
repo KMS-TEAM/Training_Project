008bc27e8ecb4217e0df63ef26bce4f35bf8fb75
lacie-life <00sao00ios00@gmail.com>
2020-07-14 22:07:08 +0700

    create webcam

---
+++ TungLam/dateAndBirth/cmake-build-debug/CMakeFiles/dateAndBirth.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dateAndBirth.dir/main.cpp.obj"
+  "dateAndBirth.exe"
+  "dateAndBirth.exe.manifest"
+  "dateAndBirth.lib"
+  "dateAndBirth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dateAndBirth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
