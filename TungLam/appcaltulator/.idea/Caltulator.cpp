6aedaf28e625bd457bf802eb81162a1e55b73644
saltySeaweed <nam8x30@gmail.com>
2020-07-21 10:08:06 +0700

    test faild

---
+++ TungLam/appcaltulator/.idea/Caltulator.cpp
@@ -0,0 +1,54 @@
+//
+// Created by NguyenTungLam on 7/21/2020.
+//
+
+#include "Caltulator.h"
+
+
+
+bool Calculator::checkZero(double number){
+    if (number == 0) {
+        return true;
+    }
+    else false;
+}
+double Calculator::add(double x, double y){
+    return x+y;
+}
+
+double Calculator::sub(double x, double y){
+    return x - y;
+}
+
+double Calculator::mutipl(double x, double y){
+    return x*y;
+}
+
+double Calculator::divi(double x, double y){
+
+    if (!checkZero(y)){
+        return x/y;
+    }
+    else {
+        std::cout << "Can't division 0" << std::endl;
+    }
+
+}
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+
+    switch (oper)
+    {
+        case '+':
+            return add(x,y);
+        case '-':
+            return sub(x,y);
+        case '*':
+            return mutipl(x,y);
+        case '/':
+            return divi(x,y);
+        default:
+            return 0.0;
+    }
+}
